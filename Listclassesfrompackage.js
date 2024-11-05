12366e2f2f951527724c8d65ca8910207943828c
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-06 00:45:35 +0530

    Create Listclassesfrompackage.js

---
+++ Listclassesfrompackage.js
@@ -0,0 +1,12 @@
+Java.perform(()=>{
+        Java.enumerateLoadedClasses({
+                onMatch : function(name, handle){
+                        if (name.includes("com.example.helloapp")){
+                                console.log(name);
+                        }
+                },
+                onComplete : function(){
+                        console.log("-- Done --")
+                }
+        });
+});
