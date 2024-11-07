d2c9a87414a54aab9d0f8af031fd2ed0d18a8eb8
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-07 17:17:25 +0530

    Update and rename DumpFuncParams.js to DumpFuncParams(And).js

--- DumpFuncParams(And).js
+++ DumpFuncParams(And).js
@@ -1,11 +1,11 @@
 Java.perform(() => {
 
-	const classa = Java.use("<Class Name>")
+	const classa = Java.use("<Class Name>");
 
   	// This is a example for a string parameter
   
 	classa.a.overload("java.lang.String").implementation = function(s){
-		console.log("\n")
+		console.log("\n");
 		console.log(s.toString());
 		return true;
 	}
