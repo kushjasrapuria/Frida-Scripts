b9019df704c8a835175f67c2872e62353925f9df
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-10 01:20:31 +0530

    Update ListFunctions.js

--- Windows/ListFunctions.js
+++ Windows/ListFunctions.js
@@ -4,6 +4,8 @@ const module = Module.load("<Program Name>");
 
 var functions = module.enumerateImports();
 
+console.log("\n");
+
 for(var i=0; i < functions.length; i++){
     console.log(functions[i].name);
 }
