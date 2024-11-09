a65f9f62b72cea3427a2995de0445c4c9bcab2dc
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-10 01:18:13 +0530

    Create ListFunctions.js

---
+++ Windows/ListFunctions.js
@@ -0,0 +1,9 @@
+// Edit for desired program for which you want to list functions for
+
+const module = Module.load("<Program Name>");
+
+var functions = module.enumerateImports();
+
+for(var i=0; i < functions.length; i++){
+    console.log(functions[i].name);
+}
