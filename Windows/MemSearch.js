d762f7dd9cf603d1136ddae5b86e6e56e838dda4
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-10 02:15:39 +0530

    Create MemSearch.js

---
+++ Windows/MemSearch.js
@@ -0,0 +1,22 @@
+// Edit with mem perms you need 
+const memoryrange = Process.enumerateRanges("<Perms>");
+
+// The string to search in ascii
+var searchStr = "<Ascii String>";
+
+for( var i=0; i<memoryrange.length; i++){
+    var memory = Memory.scan(memoryrange[i].base, memoryrange[i].size, searchStr,{
+        onMatch: function(address, size){
+            console.log(address);
+            console.log(address.readUtf8String());
+        },
+
+        onError: function(reason){
+            console.log("Error: " + reason);
+        },
+
+        onCompete: function(){
+            console.log("Pwned");
+        },
+    });
+}
