d38f006daacd8089ea4d8eef47feb2a4fe506d81
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-13 01:28:22 +0530

    Update BypassSSLPinning.js

--- Android/BypassSSLPinning.js
+++ Android/BypassSSLPinning.js
@@ -3,6 +3,8 @@ Java.perform(function() {
     var array_list = Java.use("java.util.ArrayList");
     var conscrypt = Java.use('com.android.org.conscrypt.TrustManagerImpl');
 
+    console.log("\n");
+    
     conscrypt.checkTrustedRecursive.implementation = function(certs, ocspData, tlsSctData, host, clientAuth, untrustedChain, trustAnchorChain, used) {
         var k = array_list.$new();
         return k;
