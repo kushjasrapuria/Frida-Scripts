3e47dedb09461a20092dd85ba6447743fef09516
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-13 20:38:20 +0530

    Update AntiDebugBypass.js

--- Android/AntiDebugBypass.js
+++ Android/AntiDebugBypass.js
@@ -1,4 +1,4 @@
-// frida --codeshare kushjasrapuria/antidebugbypass -f <YOUR_BINARY>
+// frida --codeshare kushjasrapuria/anti-debug-bypass -f <YOUR_BINARY>
 
 Java.perform(function() {
     var Debug = Java.use('android.os.Debug');
