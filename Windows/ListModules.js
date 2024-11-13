3bebeb6c91ea95ab18862bb7fcf277b018025bde
/x90Slide <kushjasrapuria03@gmail.com>
2024-11-13 20:46:25 +0530

    Delete Windows directory

--- Windows/ListModules.js
+++
@@ -1,9 +0,0 @@
-// frida --codeshare kushjasrapuria/windows-list-modules -f <YOUR_BINARY>
-
-const modarray = Process.enumerateModules();
-
-console.log("\n");
-
-for(var i=0; i<modarray.length; i++){
-    console.log(modarray[i].name);
-}
